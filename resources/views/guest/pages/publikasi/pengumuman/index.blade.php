04d0eeffd68b1441c9e6bf1ea1feae23c19871aa
Fakhruzaky <d1041211030@student.untan.ac.id>
2025-02-22 19:18:32 +0700

    change poto lutpi

--- resources/views/guest/pages/publikasi/pengumuman/index.blade.php
+++ resources/views/guest/pages/publikasi/pengumuman/index.blade.php
@@ -4,8 +4,8 @@
         <div class="row column-gap-3 row-gap-3 justify-content-center">
             @foreach ($announcements as $a)
                 <div class="card col-3" style="width: 18rem;">
-                    <img src="{{ $a->image_url ? asset('storage/' . $a->image_url) : asset('img/static-img/lutpi.jpg') }}"
-                        class="card-img-top" alt="">
+                    <img src="{{ $a->image_url ? asset('storage/' . $a->image_url) : asset('') }}" class="card-img-top"
+                        alt="">
 
 
                     <div class="card-body">
