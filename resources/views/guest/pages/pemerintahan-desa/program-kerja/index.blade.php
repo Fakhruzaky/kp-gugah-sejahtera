3ce3d530e1cc7464def633264916874695a8ebcd
Fakhruzaky <d1041211030@student.untan.ac.id>
2025-04-11 21:09:31 +0700

    hapus yang tidak berguna

--- resources/views/guest/pages/pemerintahan-desa/program-kerja/index.blade.php
+++ resources/views/guest/pages/pemerintahan-desa/program-kerja/index.blade.php
@@ -1,10 +1,9 @@
 @extends('guest.layouts.main')
 @section('container')
     <div class="container my-5">
-        <h1>Program Kerja</h1>
         @foreach ($progjas as $p)
             <p>
-                {{ $loop->iteration }}. {{ $p->name }} : {!! $p->description !!}
+                {{ $p->name }} : {!! $p->description !!}
             </p>
         @endforeach
 
